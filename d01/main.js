4278335e0258ff57dc96463a06302b476d14f98b
Lukasz Wargocki <lmwargocki@gmail.com>
2022-12-01 19:12:36 +0100

    offline setup

--- d01/main.js
+++ d01/main.js
@@ -1,14 +1,18 @@
-const fs = require('fs')
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+import * as fs from 'node:fs/promises'
+//const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+// require("dotenv").config({ path: "./config/.env" })
+// console.log(process.env.cookie)
+// async function getInput(){
+//   let data = await fetch("https://adventofcode.com/2022/day/1/input", {
+//     method: "post", headers: { "Cookie": process.env.cookie }
+//   })
+//   data = await data.text()
+//   console.log(data)
+//   return data
+// }
+// input = getInput()
 
-require("dotenv").config({ path: "./config/.env" })
+let fileHandle = await fs.open('./input.txt', 'r')
 
-async function getInput(){
-  let data = await fetch("https://adventofcode.com/2022/day/1/input", {
-    method: "post", headers: { "Cookie": process.env.cookie }
-  })
-  data = await data.text()
-  console.log(data)
-  return data
-}
-input = getInput()
+
+await fileHandle.close()
